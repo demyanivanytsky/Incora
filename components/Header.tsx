319f2972aade04c3c1137dcb64fee916f4c7364d
Demyan <demuanivanytsky8@gmail.com>
2024-05-22 18:48:30 +0300

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react'
+import Link from "next/link";
+
+const Header:FC = () => {
+  return (
+      <div className='flex justify-between items-center p-6 text-white text-xl bg-blue-700 h-[80px]'>
+          <h2> Discover info with us ! </h2>
+          <Link className="underline" href="/">All User</Link>
+    </div>
+
+  )
+}
+
+export default Header;
